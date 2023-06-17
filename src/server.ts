9af4741a8da033aab19543e00ca5d772dce6d79e
hashelq <105644136+hashelq@users.noreply.github.com>
2023-06-17 07:28:12 +0000

    Add io-ts types support!

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,39 @@
 import { WebSocketServer, WebSocket } from 'ws';
-import { Callback, ClientMessage, ClientMessageType, Event, Method, RequestData, ResponseData, RPCError, ServerMessage, ServerMessageType } from './types';
+import Side from './side';
+import { Event, Method } from './types';
 
 const DEFAULT_TIMEOUT = 60 * 1000;
 
-function jsonOrNull(data: string): any | null {
-    try { return JSON.parse(data) }
-    catch (e: any) { return null }
-}
-
-export default class Server {
+export default class Server<S = void> extends Side<{ id: number, socket: WebSocket, session: S }> {
     private wss: WebSocketServer;
-    private methodHandlers: Map<string, (clientID: number, clientWS: WebSocket, data: any) => object> = new Map();
-    private eventHandlers: Map<string, (clientID: number, clientWS: WebSocket, data: any) => void> = new Map();
-    private callbacks: Map<number, Callback<Error | RPCError>> = new Map();
 
     private clientIndex = 0;
-    private methodIndex = 0;
-
     public clients: Map<number, WebSocket> = new Map();
-
-    public methodTimeout: number;
-
     public onNewClient: (clientID: number, clientWS: WebSocket) => void;
+
+    public sessionInitializer: (id: number) => S;
 
     constructor({
         port,
         methodTimeout = DEFAULT_TIMEOUT,
-        onNewClient
+        onNewClient,
+        sessionInitializer,
     }: {
         port: number,
         methodTimeout?: number,
-        onNewClient?: (clientID: number, clientWS: WebSocket) => void
+        onNewClient?: (clientID: number, clientWS: WebSocket) => void,
+        sessionInitializer: (id: number) => S
     }) {
+        super({ safeMode: true, methodTimeout});
         this.wss = new WebSocketServer({ port });
-        this.methodTimeout = methodTimeout;
 
+        this.sessionInitializer = sessionInitializer;
         this.onNewClient = onNewClient;
 
         this.wss.on('connection', (ws: WebSocket) => {
             const clientID = this.clientIndex++;
+
+            const source = { id: clientID, socket: ws, session: this.sessionInitializer(clientID) };
 
             this.clients.set(clientID, ws);
 
@@ -47,7 +42,7 @@ export default class Server {
                     console.error('error', error);
                 },
 
-                'message': (message: string) => this.onMessage(clientID, ws, message),
+                'message': (message: string) => this.onMessage(message, source),
 
                 'close': () => {
                     this.clients.delete(clientID)
@@ -61,132 +56,19 @@ export default class Server {
             if (this.onNewClient)
                 this.onNewClient(clientID, ws);
         });
-    }
+    } 
 
-    private onMessageMethod(clientID: number, clientWS: WebSocket, request: RequestData) {
-        const handler = this.methodHandlers.get(request.name);
-        let payload = undefined;
-        let error = undefined;
-
-        if (!handler) {
-            error = `server method not implemented: ${request.name}`;
-        } else {
-            try {
-                payload = handler(clientID, clientWS, request.payload);
-            } catch (e: any) {
-                if (e instanceof RPCError)
-                    error = e.error;
-                else
-                    throw e;
-            }
-        }
-
-        const response: ResponseData = { index: request.index, payload, errorRPC: error };
-        const toSend: ServerMessage = {
-            type: ServerMessageType.Response,
-            content: response
-        };
-        clientWS.send(JSON.stringify(toSend));
-    }
-
-    private onMessageResponse(response: ResponseData) {
-        const callback = this.callbacks.get(response.index);
-
-        if (callback === undefined)
-            return;
-
-        if (response.errorRPC !== undefined)
-            callback.reject(new RPCError(response.errorRPC));
-        else
-            callback.resolve(response.payload);
-
-        this.callbacks.delete(response.index);
-    }
-
-    private onMessageEvent(clientID: number, ws: WebSocket, event: Event<any>) {
-        const eventHandler = this.eventHandlers.get(event.name);
-
-        if (eventHandler)
-            eventHandler(clientID, ws, event.data);
-    }
-
-    private onMessage(clientID: number, ws: WebSocket, raw: string) {
-        const message: ClientMessage | null = jsonOrNull(raw.toString());
-        if (message === null)
-            return;
-
-        const content = message.content;
-
-        switch (message.type) {
-            case ClientMessageType.Method:
-                const request = RequestData.from_object(content as any);
-                this.onMessageMethod(clientID, ws, request);
-                break;
-
-            case ClientMessageType.Response:
-                this.onMessageResponse(content as ResponseData);
-                break;
-
-            case ClientMessageType.Event:
-                this.onMessageEvent(clientID, ws, content as Event<any>);
-                break;
-
-            default:
-        }
+    public sendEvent<E extends Event<any>>(clientWS: WebSocket, event: E) {
+      return this._sendEvent(clientWS, event);
     }
 
     public call<Req, Resp, M extends Method<Req, Resp>>(clientWS: WebSocket, method: M) {
-        const data: RequestData = RequestData.from_method(method, this.methodIndex++);
-        const toSend: ServerMessage = {
-            type: ServerMessageType.Method,
-            content: data
-        };
-
-        // send
-        clientWS.send(JSON.stringify(toSend));
-
-        // wait
-        return new Promise((resolve, reject) => {
-            this.callbacks.set(data.index, {
-                resolve,
-                reject,
-                startedAt: new Date(),
-            });
-
-            // Timeout for a timeout!
-            // IDEA: clear timeout?
-            setTimeout(() => {
-                const callback = this.callbacks.get(data.index);
-                if (callback) {
-                    callback.reject(new Error('timeout'));
-
-                    this.callbacks.delete(data.index);
-                };
-            }, this.methodTimeout);
-        });
-    }
-
-    sendEvent<E extends Event<any>>(client: WebSocket, event: E) {
-        const toSend: ServerMessage = {
-            type: ServerMessageType.Event,
-            content: event
-        };
-        client.send(JSON.stringify(toSend));
-    }
+      return this._call(clientWS, method);
+    };
 
     broadcastEvent<E extends Event<any>>(event: E) {
         Array.from(this.clients).map(
             ([_, x]) => event.withs(this, x)
         );
-    }
-
-    public onMethod<Req, Res, T extends Method<Req, Res>>(signature: T, fn: (cientID: number, clientWS: WebSocket, data: Req) => Res) {
-        const name = signature.name;
-        this.methodHandlers.set(name, fn as (data: any) => any);
-    }
-
-    public onEvent<Data, T extends Event<Data>>(signature: T, fn: (clientID: number, clientWS: WebSocket, data: Data) => void) {
-        const name = signature.name;
-        this.eventHandlers.set(name, fn);
-    }
+    } 
 };
---
+++ src/side.ts
@@ -0,0 +1,202 @@
+import { RTMethodHandler, RTEventHandler, Callback, Message, MessageType, Event, Method, RequestData, ResponseData, RPCError, RequestData__RT, Message__RT, ResponseData__RT, EventData__RT, EventData } from './types';
+import { isLeft } from 'fp-ts/lib/Either';
+
+function jsonOrNull(data: string): any | null {
+    try { return JSON.parse(data) }
+    catch (e: any) { return null }
+}
+
+export default abstract class Side<CS extends { socket: WebSocket }> {
+    protected methodHandlers: Map<string, RTMethodHandler<(data: any, source: CS) => unknown>> = new Map();
+    protected eventHandlers: Map<string, RTEventHandler<(data: any, source: CS) => void>> = new Map();
+    protected callbacks: Map<number, Callback<Error | RPCError>> = new Map();
+    protected methodIndex = 0;
+
+    private safeMode: boolean;
+
+    public methodTimeout: number;
+
+    constructor({ safeMode, methodTimeout }: { safeMode: boolean, methodTimeout?: number }) {
+        this.methodTimeout = methodTimeout;
+        this.safeMode = safeMode;
+    }
+
+    private handleProtocolError(message: string) {
+      if (this.safeMode)
+        return;
+
+      throw new Error(message);
+    }
+
+    protected onMessageMethod(request: RequestData, source: CS) {
+        const handler = this.methodHandlers.get(request.name);
+        let payload = undefined;
+        let error = undefined;
+
+        if (!handler) {
+            error = `Method not implemented: ${request.name}`;
+        } else {
+            const { fn, rtRequest } = handler;
+            const dataInput = rtRequest.decode(request.payload);
+
+            if (isLeft(dataInput))
+                error = `Method payload malformed`;
+            else {
+                const input = dataInput.right;
+
+                try {
+                    payload = fn(input, source);
+                } catch (e: any) {
+                    if (e instanceof RPCError)
+                        error = e.error;
+                    else
+                        throw e;
+                }
+            }
+        }
+
+        const response: ResponseData = { index: request.index, payload, errorRPC: error };
+        const toSend: Message = {
+            type: MessageType.Response,
+            content: response
+        };
+        source.socket.send(JSON.stringify(toSend));
+    }
+
+    protected onMessageResponse(response: ResponseData) {
+        const callback = this.callbacks.get(response.index);
+
+        if (callback === undefined)
+          return this.handleProtocolError(`No callback found: ${response.index}`);
+
+        const { resolve, reject, rtResponse } = callback;
+
+        if (response.errorRPC !== undefined)
+            reject(new RPCError(response.errorRPC));
+        else {
+            const decoded = rtResponse.decode(response.payload);
+
+            if (isLeft(decoded))
+                return;
+
+            resolve(decoded.right);
+        }
+
+        this.callbacks.delete(response.index);
+    }
+
+    protected onMessageEvent(event: EventData, source: CS) {
+        const eventHandler = this.eventHandlers.get(event.name);
+        if (eventHandler) {
+            const { fn, rtData } = eventHandler;
+
+            const decoded = rtData.decode(event.data);
+            if (isLeft(decoded))
+                return this.handleProtocolError("Event body malformed.");
+
+            fn(decoded.right, source);
+        };
+    }
+
+    protected onMessage(raw: string, source: CS) {
+        const message: object | null = jsonOrNull(raw.toString());
+        if (message === null)
+            return this.handleProtocolError("Failed to parse a JSON message body.");
+
+        const decodedMsg = Message__RT.decode(message);
+        if (isLeft(decodedMsg))
+            return this.handleProtocolError("Message body malformed.");
+
+        const content = decodedMsg.right.content;
+
+        switch (decodedMsg.right.type) {
+            case MessageType.Method:
+                const decodedReq = RequestData__RT.decode(content);
+                if (isLeft(decodedReq))
+                    return this.handleProtocolError("Method body malformed.");
+
+                this.onMessageMethod(decodedReq.right, source);
+                break;
+
+            case MessageType.Response:
+                const decodedRes = ResponseData__RT.decode(content);
+                if (isLeft(decodedRes))
+                    return this.handleProtocolError("Response body malformed.");
+
+                this.onMessageResponse(decodedRes.right);
+                break;
+
+            case MessageType.Event:
+                const decodedEvt = EventData__RT.decode(content);
+                if (isLeft(decodedEvt))
+                    return this.handleProtocolError("Event body malformed.");
+
+                this.onMessageEvent(decodedEvt.right, source);
+                break;
+
+            default:
+        }
+    }
+
+    protected _sendEvent<E extends Event<any>>(socket: WebSocket, event: E) {
+        const toSend: Message = {
+            type: MessageType.Event,
+            content: event
+        };
+        socket.send(JSON.stringify(toSend));
+    }
+
+    protected _call<Req, Resp, M extends Method<Req, Resp>>(socket: WebSocket, method: M) {
+        const data: RequestData = { index: this.methodIndex++, name: method.name, payload: method.request };
+        const toSend: Message = {
+            type: MessageType.Method,
+            content: data
+        };
+
+        // send
+        socket.send(JSON.stringify(toSend));
+
+        // wait
+        return new Promise((resolve, reject) => {
+            this.callbacks.set(data.index, {
+                rtResponse: method.rtResponse,
+                resolve,
+                reject,
+                startedAt: new Date(),
+            });
+
+            // Timeout for a timeout!
+            // IDEA: clear timeout?
+            setTimeout(() => {
+                const callback = this.callbacks.get(data.index);
+                if (callback) {
+                    callback.reject(new Error('timeout'));
+
+                    this.callbacks.delete(data.index);
+                };
+            }, this.methodTimeout);
+        });
+    }
+
+    public onMethod<Req, Res>(signature: Method<Req, Res>, fn: (data: Req, source: CS) => Res) {
+        if (signature.rtRequest === undefined)
+            throw new Error("rtRequest of a method cannot be undefined!");
+
+        if (signature.rtResponse === undefined)
+            throw new Error("rtResponse of a method cannot be undefined!");
+
+        const name = signature.name;
+        this.methodHandlers.set(name, {
+            rtRequest: signature.rtRequest,
+            fn
+        });
+    }
+
+    public onEvent<Data, T extends Event<Data>>(signature: T, fn: (data: Data, source: CS) => void) {
+        if (signature.rtData === undefined)
+            throw new Error("rtData of an event cannot be undefined!");
+
+        const name = signature.name;
+        this.eventHandlers.set(name, { rtData: signature.rtData, fn });
+    }
+}
