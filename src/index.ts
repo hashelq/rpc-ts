ea701320cba8b9e4d259ea43926fb698b426f06e
hashelq <105644136+hashelq@users.noreply.github.com>
2024-02-26 04:49:23 +0000

    Add reexports for sockets/servers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import Client from './client.js';
 export * from './types.js';
 export * as iots from 'io-ts';
 
+// Reexports
+export { default as AbstractServer } from "./servers/abstract.js";
+export { default as WebSocketServerImpl } from "./servers/wss.js";
+
+export { default as Socket } from "./sockets/abstract.js";
+export { default as WebSocketImpl } from "./sockets/ws.js";
+
 export {
     Server,
     Client
